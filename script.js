7a0063757a8f9ce09b308bdc08fd91163b0b1a7e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-21 20:44:36 +0530

    changed function to async and fetched data

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const ApiKey = "8f6200216e7a219e044fb1179fea87b6";
 
 searchButton.addEventListener("click", getCityCoordinates);
 
-function getCityCoordinates() {
+async function getCityCoordinates() {
   const cityName = cityInput.value.trim();
   if (!cityName) {
     alert("CITY NAME IS EMPTY");
     return;
   }
   const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${ApiKey}`;
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  console.log(data);
 }
