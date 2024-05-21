d8c69c9184476def5557d5a9b088d55534feb7fd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-21 20:36:58 +0530

    added url of api open weather

--- script.js
+++ script.js
@@ -10,5 +10,5 @@ function getCityCoordinates() {
     alert("CITY NAME IS EMPTY");
     return;
   }
-  const GeoCodingApiUrl = ``;
+  const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${ApiKey}`;
 }
